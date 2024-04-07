1ca3d482ec99de4843afc353e7dc334c36221cc5
Alex Schmith <alexanderschmith@protonmail.com>
2024-04-07 17:57:04 -0400

    Added bubbletea components for tui

--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.21.1
 require (
 	fyne.io/fyne/v2 v2.4.1 // indirect
 	fyne.io/systray v1.10.1-0.20230722100817-88df1e0ffa9a // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/bubbles v0.18.0 // indirect
+	github.com/charmbracelet/bubbletea v0.25.0 // indirect
+	github.com/charmbracelet/lipgloss v0.9.1 // indirect
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/fredbi/uri v1.0.0 // indirect
@@ -19,9 +24,17 @@ require (
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.6 // indirect
 	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
 	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
@@ -30,7 +43,9 @@ require (
 	golang.org/x/image v0.11.0 // indirect
 	golang.org/x/mobile v0.0.0-20230531173138-3c911d8e3eda // indirect
 	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
--- pomodoro.go
+++ pomodoro.go
@@ -1,72 +1,46 @@
-package main
-
-import (
-    "fmt"
-    "time"
-    "fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
-)
-
-var (
-	startButton         *widget.Button
-	startShortBreakButton *widget.Button
-	startLongBreakButton  *widget.Button
-	ticker              *time.Ticker
-)
-
-func main() {
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Pomodoro Timer")
-
-	timerLabel := widget.NewLabel("25:00")
-	startButton := widget.NewButton("Start Pomodoro", func() {
-		go pomodoroTimer(timerLabel, 25*time.Minute)
-		startButton.Disable()
-	})
-	startShortBreakButton := widget.NewButton("Start Short Break", func() {
-		go pomodoroTimer(timerLabel, 5*time.Minute)
-		startShortBreakButton.Disable()
-	})
-	startLongBreakButton := widget.NewButton("Start Long Break", func() {
-		go pomodoroTimer(timerLabel, 15*time.Minute)
-		startLongBreakButton.Disable()
-	})
-	stopButton := widget.NewButton("Stop", func() {
-		stopTimer()
-		startButton.Enable()
-		startShortBreakButton.Enable()
-		startLongBreakButton.Enable()
-	})
-
-	buttonContainer := container.NewHBox(startButton, startShortBreakButton, startLongBreakButton, stopButton)
-	content := container.NewVBox(timerLabel, buttonContainer)
-
-	myWindow.SetContent(content)
-	myWindow.ShowAndRun()
-}
 
 
-func pomodoroTimer(timerLabel *widget.Label, duration time.Duration) {
-	ticker = time.NewTicker(duration)
 
-	for remainingTime := duration; remainingTime > 0; remainingTime -= time.Second {
-		select {
-		case <-ticker.C:
-			timerLabel.SetText("00:00")
-			// You can add a notification here, e.g., displaying a message box or playing a sound
-			break
-		default:
-			timerLabel.SetText(fmt.Sprintf("%02d:%02d", remainingTime.Minutes(), int(remainingTime.Seconds())%60))
-			time.Sleep(1 * time.Second)
-		}
+
+func main(){
+
+    m := model{
+		timer: timer.NewWithInterval(timeout, time.Millisecond),
+		keymap: keymap{
+			start: key.NewBinding(
+				key.WithKeys("s"),
+				key.WithHelp("s", "start"),
+			),
+			stop: key.NewBinding(
+				key.WithKeys("s"),
+				key.WithHelp("s", "stop"),
+			),
+			reset: key.NewBinding(
+				key.WithKeys("r"),
+				key.WithHelp("r", "reset"),
+			),
+			quit: key.NewBinding(
+				key.WithKeys("q", "ctrl+c"),
+				key.WithHelp("q", "quit"),
+			),
+		},
+		help: help.New(),
+	}
+	m.keymap.start.SetEnabled(false)
+
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Uh oh, we encountered an error:", err)
+		os.Exit(1)
 	}
 
-	stopTimer()
-}
 
-func stopTimer() {
-	if ticker != nil {
-		ticker.Stop()
-	}
+
+
+
+
+
+
+
+
+
 }
